46775f5bbfbd5f91009ecc6a55ab450ff43bbfb9
Chris O'Reilly <oreillc194@potsdam.edu>
2016-12-27 20:26:46 -0500

    Allow alternate spellings

--- run.py
+++ run.py
@@ -1,46 +1,51 @@
 import random
 
 alphabet = {
-    'a': 'alfa',
-    'b': 'bravo',
-    'c': 'charlie',
-    'd': 'delta',
-    'e': 'echo',
-    'f': 'foxtrot',
-    'g': 'golf',
-    'h': 'hotel',
-    'i': 'india',
-    'j': 'juliett',
-    'k': 'kilo',
-    'l': 'lima',
-    'm': 'mike',
-    'n': 'november',
-    'o': 'oscar',
-    'p': 'papa',
-    'q': 'quebec',
-    'r': 'romeo',
-    's': 'sierra',
-    't': 'tango',
-    'u': 'uniform',
-    'v': 'victor',
-    'w': 'whiskey',
-    'x': 'x-ray',
-    'y': 'yankee',
-    'z': 'zulu',
+    'a': ('alfa', 'alpha'),
+    'b': ('bravo', ),
+    'c': ('charlie', ),
+    'd': ('delta', ),
+    'e': ('echo', ),
+    'f': ('foxtrot', ),
+    'g': ('golf', ),
+    'h': ('hotel', ),
+    'i': ('india', ),
+    'j': ('juliett', 'juliet'),
+    'k': ('kilo', ),
+    'l': ('lima', ),
+    'm': ('mike', ),
+    'n': ('november', ),
+    'o': ('oscar', ),
+    'p': ('papa', ),
+    'q': ('quebec', ),
+    'r': ('romeo', ),
+    's': ('sierra', ),
+    't': ('tango', ),
+    'u': ('uniform', ),
+    'v': ('victor', ),
+    'w': ('whiskey', ),
+    'x': ('x-ray', 'xray'),
+    'y': ('yankee', ),
+    'z': ('zulu', ),
 }
 
 def infinite_shuffle(items):
     while True:
         yield from random.sample(items, len(items))
 
+def check_response(response, answers):
+    def normalize(str):
+        return str.strip().lower()
+    return normalize(response) in map(normalize, answers)
+
 if __name__ == '__main__':
-    for letter, code_word in infinite_shuffle(alphabet.items()):
+    for letter, code_words in infinite_shuffle(alphabet.items()):
         try:
-            answer = input(letter.upper() + ': ')
-            if answer.strip().lower() == code_word:
+            response = input(letter.upper() + ': ')
+            if check_response(response, code_words):
                 print('Right')
             else:
-                print('Wrong:', code_word.title())
+                print('Wrong:', code_words[0].title())
             print()
         except EOFError:
             # quit gracefully
